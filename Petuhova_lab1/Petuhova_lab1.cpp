7165ea004743acb4364b9f0ee25ef8fe606c01a4
Mari <marjasha01@gmail.com>
2024-09-10 11:46:30 +0300

    comit

---
+++ Petuhova_lab1/Petuhova_lab1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class pipe {
+    int kilometr;
+    int length;
+    int diametr;
+    bool repair;
+};
+
+class ks {
+    string name;
+    int workshops;
+
+
+};
+
+int main()
+{
+    
+}
