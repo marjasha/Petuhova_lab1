5cafef023159c6bfdecd26a4bdd9df2f0bd14910
Mari <marjasha01@gmail.com>
2024-09-22 00:50:05 +0300

    entering numbers

--- Petuhova_lab1/Petuhova_lab1.cpp
+++ Petuhova_lab1/Petuhova_lab1.cpp
@@ -1,5 +1,7 @@
 ﻿#include <iostream>
 #include <string>
+#include <typeinfo>
+
 using namespace std;
 
 class pipe {
@@ -18,5 +20,23 @@ class ks {
 
 int main()
 {
-    
+    int f;
+    setlocale(LC_ALL, ""); //для отображение русского языка
+    cout << "Введите: "<<endl;
+    cout << "Добавить трубу - 1"<<endl;
+    cout << "Добавить КС - 2"<<endl;
+    cout << "Просмотр всех объектов - 3"<<endl;
+    cout << "Редактировать трубу - 4" << endl;
+    cout << "Редактировать КС - 5" << endl;
+    cout << "Сохранить - 6" << endl;
+    cout << "Загрузить - 7" << endl;
+    cout << "Выход - 0" << endl;
+    cin >> f;
+    while (!(cin >> f)) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');// отчистила ввод
+        cout << "Введите число!" << endl;
+        cin >> f;
+    }
+
 }
