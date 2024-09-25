558ea884c2b486aca7ae2445f88d73ec38331ff2
Mari <marjasha01@gmail.com>
2024-09-25 19:18:21 +0300

    + функция

--- Petuhova_lab1/Petuhova_lab1.cpp
+++ Petuhova_lab1/Petuhova_lab1.cpp
@@ -20,9 +20,9 @@ struct ks {
 
 void check_command(int& x) {
     cin >> x;
-    while ((x> 7) || (x < 0) || !(x)) {
+    while ((x> 7) || (x < 0)) {
         cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');// отчистила ввод
+        cin.ignore(10000, '\n');// отчистила ввод
         cout << "Введите число от 0 до 7!" << endl;
         cin >> x;
     }
@@ -30,9 +30,9 @@ void check_command(int& x) {
 
 void check_int(int& x) {
     cin >> x;
-    while (!x || x < 0 || cin.peek() != '\n' || cin.peek() == ' ') {
+    while (!x || x < 0) {
         cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');// отчистила ввод
+        cin.ignore(10000, '\n');// отчистила ввод
         cout << "Введите число!" << endl;
         cin >> x;
     }
@@ -40,9 +40,9 @@ void check_int(int& x) {
 
 void check_bool(bool& x) {
     cin >> x;
-    while (!(x) || (x < 0) || (x > 1)) {
+    while (cin.fail() || x < 0) {
         cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cin.ignore(10000, '\n');
         cout << "Труба в ремонте? (1 - да, 0 - нет)"<<endl;
         cin >> x;
 
@@ -53,7 +53,7 @@ void check_double(double& x) {
     cin >> x;
     while (!x || (x < 0)) {
         cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cin.ignore(10000, '\n');
         cout << "Введите эффективность!" << endl;
         cin >> x;
     }
@@ -97,6 +97,30 @@ ks ks_input() {
     return newks;
 }
 
+void pipe_out(const pipe& newpipe) {  // const не позволяет изменять данные
+    if (newpipe.kilometr.empty()) {
+        cout << "Нет данных" <<endl;
+    }
+    else {
+        cout << "Имя " << newpipe.kilometr <<endl;
+        cout << "Длинна " << newpipe.length <<endl;
+        cout << "Диаметр " << newpipe.diametr<<endl;
+        cout << "В ремонте " << newpipe.repair <<endl;
+    }
+}
+
+void ks_out(const ks& newks) {  // const не позволяет изменять данные
+    if (newks.name.empty()) {
+        cout << "Нет данных" << endl;
+    }
+    else {
+        cout << "Имя " << newks.name << endl;
+        cout << "Количество цехов " << newks.workshops << endl;
+        cout << "Количество цехов в работе " << newks.workshops_in_work << endl;
+        cout << "Эффективность " << newks.eff << endl;
+    }
+}
+
 int main()
 {
     int f;
@@ -104,16 +128,18 @@ int main()
     ks newks;
 
     setlocale(LC_ALL, ""); //для отображение русского языка
-    cout << "Введите: "<<endl;
-    cout << "Добавить трубу - 1"<<endl;
-    cout << "Добавить КС - 2"<<endl;
-    cout << "Просмотр всех объектов - 3"<<endl;
-    cout << "Редактировать трубу - 4" << endl;
-    cout << "Редактировать КС - 5" << endl;
-    cout << "Сохранить - 6" << endl;
-    cout << "Загрузить - 7" << endl;
-    cout << "Выход - 0" << endl;
-
+    while (true) {
+        cout << "Введите: " << endl;
+        cout << "Добавить трубу - 1" << endl;
+        cout << "Добавить КС - 2" << endl;
+        cout << "Просмотр всех объектов - 3" << endl;
+        cout << "Редактировать трубу - 4" << endl;
+        cout << "Редактировать КС - 5" << endl;
+        cout << "Сохранить - 6" << endl;
+        cout << "Загрузить - 7" << endl;
+        cout << "Выход - 0" << endl;
+   
+    
     check_command(f);
 
     switch (f) {
@@ -126,7 +152,10 @@ int main()
         break;
     }
     case 3: {
-
+        cout << "Трубы" << endl;
+        pipe_out(newpipe);
+        cout << "КС" << endl;
+        ks_out(newks);
         break;
     }
     case 4: {
@@ -147,6 +176,7 @@ int main()
     }
     case 0: {
         return 0;
+    }
     }
     }
 
